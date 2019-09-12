9948377a5f28edd289bff5cfc6da884b336e362e
Juan Marin <juanmarin@Work-MacBook-Pro.local>
2019-09-11 20:36:24 -0700

    Added Validation

--- server.py
+++ server.py
@@ -1,24 +1,34 @@
-from flask import Flask, render_template, request, redirect
+from flask import Flask, render_template, request, redirect, session, flash
+import re
+
+EMAIL_REGEX = re.compile(r'^[a-zA-Z0-9.+_-]+@[a-zA-Z0-9._-]+\.[a-zA-Z]+$')
 
 app = Flask(__name__)
 
-@app.route('/')
+app.secret_key = "secret"
+
+@app.route('/', methods=['GET'])
 def index():
     return render_template('index.html')
 
 @app.route('/results', methods=['POST'])
 def results():
-    first_name = request.form['first_name']
-    print(first_name)
-
-    last_name = request.form['last_name']
-    print(last_name)
-
-    location = request.form['location']
-    print(location)
-
-    comment = request.form['comment']
-    print(comment)
-
-    return render_template('result.html', first_name=first_name, last_name=last_name, location=location, comment=comment)
+    if len(request.form['first_name']) < 1:
+        flash("First name cannot be blank.")
+        print("First name cannot be blank.")
+        return redirect('/')
+    if len(request.form['last_name']) < 1:
+        flash("Last name cannot be blank.")
+        print("Last name cannot be blank.")
+        return redirect('/')
+    if len(request.form['email']) < 1:
+        flash("Email cannot be blank.")
+        print("Email cannot be blank.")
+        return redirect('/')
+    elif not EMAIL_REGEX.match(request.form['email']):
+        flash("Invalid email address.")
+        print("Invalid email address.")
+        return redirect('/')
+    else:
+        return render_template('result.html')
 app.run(debug=True)
